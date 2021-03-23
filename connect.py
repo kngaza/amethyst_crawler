408280a6df2abcd422b526d38157e6ec8280a6d3
VietNDuc <viet.nguyen1123@gmail.com>
2021-03-23 11:16:38 +0700

    Update params to connect azuresql

--- connect.py
+++ connect.py
@@ -13,12 +13,17 @@ def get_azure_engine():
     params = urllib.parse.quote_plus \
         (f'Driver={os.getenv("DRIVER")};'
         f'Server=tcp:{os.getenv("SERVER")},1433;'
-        f'Database=fiin_db;'
-        f'Uid={os.getenv("USER")};'
+        f'Database={os.getenv("DATABASE")};'
+        f'Uid={os.getenv("USERNAME")};'
         f'Pwd={os.getenv("PASSWORD")};'
         f'Encrypt=yes;'
         f'TrustServerCertificate=no;Connection Timeout=30;')
     conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-    engine_azure = create_engine(conn_str, echo=True)
+    engine_azure = create_engine(conn_str)
 
     return engine_azure
+
+
+if __name__ == "__main__":
+    engine = get_azure_engine()
+    print(engine.table_names())
